3613d4c1f04b6927f233195c17ca164c32891b67
David Mead <2652016m@student.gla.ac.uk>
2025-10-01 21:27:50 +0100

    Creating Basic UART project in vivado

---
+++ Foundation/Basic UART operation/Basic UART operation.srcs/sources_1/new/top.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: David Mead
+// 
+// Create Date: 30.09.2025 21:21:31
+// Design Name: Basic UART operation
+// Module Name: top
+// Project Name: FPGA Foundations
+// Description: 
+//      Get a basic transmission and recieve working from FPGA to Laptop using UART
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    input clk
+    );
+endmodule
